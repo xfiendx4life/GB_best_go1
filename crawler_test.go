1b478c3732a8dd5538c4ec327722bbd13a326719
Kirill <xfiendx4life@gmail.com>
2021-11-23 17:31:43 +0300

    changeDepth test added

--- crawler_test.go
+++ crawler_test.go
@@ -18,7 +18,7 @@ import (
 )
 
 type crawlerStub struct {
-	r         Requester
+	r         requesterMock
 	res       chan CrawlResult
 	chngDepth chan int // для изменения глубины поиска
 	visited   map[string]struct{}
@@ -26,21 +26,42 @@ type crawlerStub struct {
 	testFunc  func() []CrawlResult
 }
 
-type requesterStub struct{}
+type requesterMock struct {
+	requestCounter int // to cunt number of requests
+	returnsError   bool
+}
 
-func (r *requesterStub) Get(ctx context.Context, url string) (Page, error) {
-	ps := pageStub{}
+func (r *requesterMock) Get(ctx context.Context, url string) (Page, error) {
+	r.requestCounter += 1
+	if r.returnsError {
+		r.returnsError = false
+		return nil, fmt.Errorf("test error")
+	}
+	ps := pageStub{
+		requestCounter: r.requestCounter,
+	}
 	return &ps, nil
 }
 
-type pageStub struct{}
+type pageStub struct {
+	requestCounter int // sent from requester
+}
 
 func (p *pageStub) GetTitle(ctx context.Context) string {
-	return "stubPageTitle"
+	// as crawler uses interface that's the way to send counter to test function
+	return fmt.Sprintf("stubPageTitle%d", p.requestCounter)
 }
 
 func (p *pageStub) GetLinks(ctx context.Context) []string {
-	return make([]string, 0)
+
+	return []string{
+		"firstlink",
+		"secondlink",
+		"thirdlink",
+		"fourthlink",
+		"fifthlink",
+		"sixthlink",
+	}[:p.requestCounter]
 }
 
 // как тут изменить??? чтобы вариации сделать??
@@ -143,7 +164,7 @@ func TestProcessResultMaxErrorsCancelFunc(t *testing.T) {
 		Url:        "http://127.0.0.1:5500/1.html",
 		Timeout:    3,
 	}
-	// we have 3 errors 
+	// we have 3 errors
 	c := crawlerStub{
 		res: make(chan CrawlResult),
 		testFunc: func() []CrawlResult {
@@ -159,7 +180,7 @@ func TestProcessResultMaxErrorsCancelFunc(t *testing.T) {
 	cancelChecker := false
 	cancePointer := &cancelChecker
 	go c.Scan(ctx, "url", 1) // send data to chan
-	// wrap cancel func 
+	// wrap cancel func
 	processResult(ctx, func() {
 		cancel()
 		*cancePointer = true
@@ -167,16 +188,15 @@ func TestProcessResultMaxErrorsCancelFunc(t *testing.T) {
 	require.True(t, cancelChecker)
 }
 
-
 func TestProcessResultMaxResultsCancelFunc(t *testing.T) {
 	cfg := Config{
 		MaxDepth:   10,
 		MaxResults: 1, // set 1 max result
-		MaxErrors:  1, 
+		MaxErrors:  1,
 		Url:        "http://127.0.0.1:5500/1.html",
 		Timeout:    3,
 	}
-	// we have 3 results 
+	// we have 3 results
 	c := crawlerStub{
 		res: make(chan CrawlResult),
 		testFunc: func() []CrawlResult {
@@ -192,10 +212,156 @@ func TestProcessResultMaxResultsCancelFunc(t *testing.T) {
 	cancelChecker := false
 	cancePointer := &cancelChecker
 	go c.Scan(ctx, "url", 1) // send data to chan
-	// wrap cancel func 
+	// wrap cancel func
 	processResult(ctx, func() {
 		cancel()
 		*cancePointer = true
 	}, &c, cfg)
 	require.True(t, cancelChecker)
+}
+
+func TestScanDepth1(t *testing.T) {
+	cfg := Config{
+		MaxDepth:   1,
+		MaxResults: 1, // set 1 max result
+		MaxErrors:  1,
+		Url:        "http://testUrl/1.html",
+		Timeout:    100,
+	}
+	r := requesterMock{
+		requestCounter: 0,
+	}
+	cr := NewCrawler(&r)
+	ctx := context.Background()
+	ctx, _ = context.WithTimeout(ctx, time.Second*1)
+	go cr.Scan(ctx, cfg.Url, cfg.MaxDepth)
+	select {
+	case <-ctx.Done():
+		require.True(t, false)
+		return
+	case msg := <-cr.ChanResult():
+		assert.Nil(t, msg.Err)
+		assert.Equal(t, msg.Title, "stubPageTitle1")
+	}
+}
+
+func TestScanCheckDepth(t *testing.T) {
+	cfg := Config{
+		MaxDepth: 3,
+		// set 3 max result equals to depth,
+		// either for loop waits for more
+		MaxResults: 3,
+		MaxErrors:  1,
+		Url:        "http://testUrl/1.html",
+		Timeout:    100,
+	}
+	r := requesterMock{
+		requestCounter: 0,
+	}
+	cr := NewCrawler(&r)
+	ctx := context.Background()
+	// ctx, _ = context.WithTimeout(ctx, time.Second*1)
+	go cr.Scan(ctx, cfg.Url, cfg.MaxDepth)
+	res := ""
+	_ = res
+	var err error
+	func() {
+		for {
+			select {
+			case <-ctx.Done():
+				require.True(t, false)
+				return
+			case msg := <-cr.ChanResult():
+				if cfg.MaxResults > 0 {
+					cfg.MaxResults--
+					res = msg.Title
+					err = msg.Err
+				} else {
+					return
+				}
+			}
+		}
+	}()
+
+	assert.Nil(t, err)
+	assert.NotEqual(t, res, "")
+	assert.Equal(t, res, "stubPageTitle3")
+}
+
+func TestScanError(t *testing.T) {
+	cfg := Config{
+		MaxDepth: 3,
+		// set 3 max result equals to depth,
+		// either for loop waits for more
+		MaxResults: 3,
+		MaxErrors:  3,
+		Url:        "http://testUrl/1.html",
+		Timeout:    100,
+	}
+	r := requesterMock{
+		requestCounter: 0,
+		returnsError:   true,
+	}
+	cr := NewCrawler(&r)
+	ctx := context.Background()
+	// ctx, _ = context.WithTimeout(ctx, time.Second*1)
+	go cr.Scan(ctx, cfg.Url, cfg.MaxDepth)
+	var err error
+	select {
+	case <-ctx.Done():
+		require.True(t, false)
+		return
+	case msg := <-cr.ChanResult():
+		err = msg.Err
+	}
+	require.NotNil(t, err)
+	assert.Equal(t, err.Error(), "test error")
+}
+
+func TestScanCheckChangingDepth(t *testing.T) {
+	cfg := Config{
+		MaxDepth: 1,
+		// crawler works recursive, thats why we get only "http://testUrl/1.html"
+		// if we set maxdepth = 1
+		// but we add 2 more of depth, so we got 3 results
+		// because pageStube.get() returns
+		// [firstlink]
+		// [firstlink, secondlink]
+		// [firstlink, secondlink, thirdlink]
+		MaxResults: 3,
+		MaxErrors:  1,
+		Url:        "http://testUrl/1.html",
+		Timeout:    100,
+	}
+	r := requesterMock{
+		requestCounter: 0,
+		returnsError:   false,
+	}
+	cr := NewCrawler(&r)
+	ctx := context.Background()
+	// ctx, _ = context.WithTimeout(ctx, time.Second*1)
+	go cr.Scan(ctx, cfg.Url, cfg.MaxDepth)
+	res := []string{}
+	_ = res
+	cr.ChangeDepth(2)
+	func() {
+		for cfg.MaxResults > 0{
+			select {
+			case <-ctx.Done():
+				require.True(t, false)
+				return
+			case msg := <-cr.ChanResult():
+				// we have to send signal to increment depth, so
+				// we want error first and if we get it we send signal
+				if cfg.MaxResults > 0 {
+					cfg.MaxResults--
+					res = append(res, msg.Title)
+				} else {
+					return
+				}
+			}
+		}
+	}()
+	assert.NotEqual(t, res, "")
+	assert.Equal(t, 3, len(cr.visited))
 }
