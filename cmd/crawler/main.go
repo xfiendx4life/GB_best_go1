3ad308404c1e45ff9975de5e874bbb4aeefdea88
xfiendx4life <xfiendx4life@gmail.com>
2021-11-28 22:44:03 +0300

    new structure. Tests are not structured

---
+++ cmd/crawler/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"lesson1/pkg/config"
+	"lesson1/pkg/crawler"
+	"lesson1/pkg/process"
+	"lesson1/pkg/requester"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func mainStarter() {
+	cfg := config.Config{}
+	err := cfg.ReadConfigFromFile(os.Getenv("CONFIGPATH"))
+	if err != nil {
+		log.Fatalf("can't create config %s", err)
+	}
+	var cr crawler.Crawler
+
+	r := requester.NewRequester(time.Duration(cfg.Timeout) * time.Second)
+	cr = crawler.NewCrawler(r)
+
+	ctx, _ := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(cfg.Timeout)) // добавим таймаут в контекст
+	go cr.Scan(ctx, cfg.Url, cfg.MaxDepth)                                          //Запускаем краулер в отдельной рутине
+	go process.ProcessResult(ctx, cancel, cr, cfg)                                  //Обрабатываем результаты в отдельной рутине
+
+	sigCh := make(chan os.Signal, 1)                      //Создаем канал для приема сигналов
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGUSR1) //Подписываемся на сигнал SIGINT
+	for {
+		select {
+		case <-ctx.Done(): //Если всё завершили - выходим
+			log.Print("ended with context")
+			return
+		case sign := <-sigCh:
+			switch sign {
+			case syscall.SIGINT:
+				cancel() //Если пришёл сигнал SigInt - завершаем контекст
+			case syscall.SIGUSR1:
+				cr.ChangeDepth(2)
+			}
+		}
+	}
+}
+
+func main() {
+	mainStarter()
+}
